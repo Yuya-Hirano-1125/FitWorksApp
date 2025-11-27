b8981e01d084482df80b148eac1fb669dd335425
katup <katup@DESKTOP-3FHKAP7>
2025-11-27 18:47:20 +0900

    炎タイプ水タイプの詳細画面に遷移できるように変更しました、dolphy.htmlがdoiphy.htmlだったので名前を変更しました

--- src/main/java/com/example/demo/controller/CharacterController.java
+++ src/main/java/com/example/demo/controller/CharacterController.java
@@ -8,15 +8,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/characters")
 public class CharacterController {
 
+	//キャラクター解放画面へ遷移
     @GetMapping("/storage")
     public String showCharacterStorage() {
         return "characters/Storage"; 
     }
     
+    //★１炎タイプの卵
+    @GetMapping("/dracoegg") 
+    public String DracoEggInfo() {
+        return "characters/dracoegg"; 
+    }
+    
+    //★2炎タイプの1進化目
     @GetMapping("/draco") 
-    public String dracoInfo() {
-        // 【修正】フォルダ階層を含めて指定する必要があります
-        // templates/characters/draco.html を読み込む場合
+    public String DracoInfo() {
         return "characters/draco"; 
+    }
+    
+    //★3炎タイプの2進化目
+    @GetMapping("/dracos") 
+    public String DracosInfo() {
+        return "characters/dracos"; 
+    }
+    
+  //★4炎タイプの最終進化
+    @GetMapping("/dragonoid") 
+    public String DragonoidInfo() {
+        return "characters/dragonoid"; 
+    }
+  //★１水タイプの卵
+    @GetMapping("/dollyegg") 
+    public String DollyEggInfo() {
+        return "characters/dollyegg"; 
+    }
+  //★2水タイプの1進化目
+    @GetMapping("/dolly") 
+    public String DollyInfo() {
+        return "characters/dolly"; 
+    }
+  //★3水タイプの2進化目
+    @GetMapping("/dolphy") 
+    public String DolphyInfo() {
+        return "characters/dolphy"; 
+    }
+  //★4水タイプの最終進化
+    @GetMapping("/dolphinas") 
+    public String DolphinasInfo() {
+        return "characters/dolphinas"; 
     }
 }
