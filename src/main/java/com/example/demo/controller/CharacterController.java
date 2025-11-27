fce605183e956d6703929ab6609b6faf70cd9e35
katupan <katupan5250@gmail.com>
2025-11-27 11:42:08 +0900

    キャラクター詳細画面に遷移できるよう変更

--- src/main/java/com/example/demo/controller/CharacterController.java
+++ src/main/java/com/example/demo/controller/CharacterController.java
@@ -3,11 +3,20 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @RequestMapping("/characters")
 public class CharacterController {
-    @GetMapping("/storage") // URL: /characters/storage
+
+    @GetMapping("/storage")
     public String showCharacterStorage() {
-        return "characters/Storage"; // テンプレート名: src/main/resources/templates/characters/Storage.html
+        return "characters/Storage"; 
+    }
+    
+    @GetMapping("/draco") 
+    public String dracoInfo() {
+        // 【修正】フォルダ階層を含めて指定する必要があります
+        // templates/characters/draco.html を読み込む場合
+        return "characters/draco"; 
     }
 }
